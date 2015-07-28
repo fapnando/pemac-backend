e134aa319da8b94b85927669c5b8ba1b7ac45e43
Fernando Alexandre <fapnando@gmail.com>
2015-07-27 21:35:04 -0300

    atualizações

--- application/config/database.php
+++ application/config/database.php
@@ -5,7 +5,7 @@ $active_record = TRUE;
 
 $db['local']['hostname'] = 'localhost';
 $db['local']['username'] = 'root';
-$db['local']['password'] = 'root';
+$db['local']['password'] = '';
 $db['local']['database'] = 'pemac';
 $db['local']['dbdriver'] = 'mysql';
 $db['local']['dbprefix'] = '';
--- application/modules/contato/config/config.php
+++ application/modules/contato/config/config.php
@@ -20,20 +20,52 @@
     
     $config['campos'] = array();
 
-    $config['campos']['ativo']['id']                          = 'ativo';
-    $config['campos']['ativo']['type']                        = 'onoff';
-    $config['campos']['ativo']['hidden']                      = false;
-    $config['campos']['ativo']['txt']                         = 'Ativo';
-    $config['campos']['ativo']['required']                    = true;
-    $config['campos']['ativo']['listagem']                    = true;
+    $config['campos']['nome'] = array();
+    $config['campos']['nome']['id']                         = 'nome';
+    $config['campos']['nome']['type']                       = 'text';
+    $config['campos']['nome']['hidden']                     = false;
+    $config['campos']['nome']['txt']                        = 'Nome';
+    $config['campos']['nome']['required']                   = true;
+    $config['campos']['nome']['listagem']                   = true;
 
-    $config['campos']['titulo'] = array();
-    $config['campos']['titulo']['id']                         = 'titulo';
-    $config['campos']['titulo']['type']                       = 'text';
-    $config['campos']['titulo']['hidden']                     = false;
-    $config['campos']['titulo']['txt']                        = 'Título';
-    $config['campos']['titulo']['required']                   = true;
-    $config['campos']['titulo']['listagem']                   = true;
+    $config['campos']['email'] = array();
+    $config['campos']['email']['id']                         = 'email';
+    $config['campos']['email']['type']                       = 'text';
+    $config['campos']['email']['hidden']                     = false;
+    $config['campos']['email']['txt']                        = 'Email';
+    $config['campos']['email']['required']                   = true;
+    $config['campos']['email']['listagem']                   = true;
+
+    $config['campos']['telefone'] = array();
+    $config['campos']['telefone']['id']                         = 'telefone';
+    $config['campos']['telefone']['type']                       = 'text';
+    $config['campos']['telefone']['hidden']                     = false;
+    $config['campos']['telefone']['txt']                        = 'Telefone';
+    $config['campos']['telefone']['required']                   = true;
+    $config['campos']['telefone']['listagem']                   = true;
+
+    $config['campos']['assunto'] = array();
+    $config['campos']['assunto']['id']                         = 'assunto';
+    $config['campos']['assunto']['type']                       = 'text';
+    $config['campos']['assunto']['hidden']                     = false;
+    $config['campos']['assunto']['txt']                        = 'Assunto';
+    $config['campos']['assunto']['required']                   = true;
+    $config['campos']['assunto']['listagem']                   = true;
+
+    $config['campos']['empresa'] = array();
+    $config['campos']['empresa']['id']                         = 'empresa';
+    $config['campos']['empresa']['type']                       = 'text';
+    $config['campos']['empresa']['hidden']                     = false;
+    $config['campos']['empresa']['txt']                        = 'Empresa';
+    $config['campos']['empresa']['required']                   = true;
+    $config['campos']['empresa']['listagem']                   = true;
+
+    $config['campos']['mensagem'] = array();
+    $config['campos']['mensagem']['id']                       = 'mensagem';
+    $config['campos']['mensagem']['type']                     = 'wys';
+    $config['campos']['mensagem']['hidden']                   = false;
+    $config['campos']['mensagem']['txt']                      = 'Mensagem';
+    $config['campos']['mensagem']['required']                 = false;
     
     //######################| FILTROS |####################################
     
@@ -50,7 +82,12 @@
     `ativo` int(255) DEFAULT NULL,
     `order` int(255) DEFAULT NULL,
     
-    `titulo` 				longtext COLLATE latin1_general_ci DEFAULT NULL,
+    `nome` 				varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
+    `email`                varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
+    `telefone`                varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
+    `assunto`                varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
+    `empresa`                varchar(255) COLLATE latin1_general_ci DEFAULT NULL,
+    `mensagem`              longtext COLLATE latin1_general_ci DEFAULT NULL,
     
     PRIMARY KEY (`id`)
     ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=0;";
